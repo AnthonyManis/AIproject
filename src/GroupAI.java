3f35207fa1dfdce32ec36c5462ce4dd4cbcab1e4
AnthonyManis <anthonygmanis@gmail.com>
2016-10-23 14:51:58 -0700

    Started making window scanning algorithm

--- src/GroupAI.java
+++ src/GroupAI.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class GroupAI extends CKPlayer {
 
-    private Point bestPoint = new Point(); // (i, j)
+	private Point bestPoint = new Point(); // (i, j)
 
 	public GroupAI(byte player, BoardModel state) {
 		super(player, state);
@@ -17,14 +17,14 @@ public class GroupAI extends CKPlayer {
 	public Point getMove(BoardModel state) {
 
 		while (state.hasMovesLeft()){
-				search(state, 3, player);
-				return new Point(bestPoint.x, bestPoint.y);
+			search(state, 3, player);
+			return new Point(bestPoint.x, bestPoint.y);
 		}
 		return null;
 	}
 
 	public int heuristic(BoardModel state){
-		List<Integer> ret = winningSpaces(state);
+		List<Integer> ret = turnsToWin(state);
 		if ( player == 1)
 			return ( ret.get(0) - ret.get(1));
 		else
@@ -35,143 +35,239 @@ public class GroupAI extends CKPlayer {
 		return (byte) p == (byte) 1 ? (byte) 2 : (byte) 1;
 	}
 
-	// player can be 1 or 2
+	// move can be 1 or 2
 	public int search(BoardModel state, int depth, byte move) {
-	    // base case
-	    if ( depth == 0 ) {
-	        return heuristic(state);
-	    }
+		// base case
+		if ( depth == 0 ) {
+			return heuristic(state);
+		}
 
-	    int bestValue = 0;
-	    for ( int i  = 0 ; i  < state.getWidth() ; i++) {
-	        for (int j = 0 ; j < state.getHeight(); j++ ) {
-	            if (state.getSpace(i, j) == 0) {
-	            	Point p = new Point(i,j);
-	                int value = search(state.placePiece(p, move), depth-1, nextPlayer(move));
-	                if ( move == 1 ) {
-	                    if ( value > bestValue){
-	                        bestValue = value;
-	                        bestPoint.x = i;
-	                        bestPoint.y = j;
-	                    }
-	                }
-	                else {
-	                    if ( value < bestValue) {
-	                        bestValue = value;
-	                        bestPoint.x = i;
-	                        bestPoint.y = j;
-	                    }
-	                }
-	            }
+		int bestValue = 0;
+		for ( int i  = 0 ; i  < state.getWidth() ; i++) {
+			for (int j = 0 ; j < state.getHeight(); j++ ) {
+				if (state.getSpace(i, j) == 0) {
+					Point p = new Point(i,j);
+					int value = search(state.placePiece(p, move), depth-1, nextPlayer(move));
+					if ( move == 1 ) {
+						if ( value > bestValue){
+							bestValue = value;
+							bestPoint.x = i;
+							bestPoint.y = j;
+						}
+					}
+					else {
+						if ( value < bestValue) {
+							bestValue = value;
+							bestPoint.x = i;
+							bestPoint.y = j;
+						}
+					}
+				}
 
-	        }
-	    }
-	    return bestValue;
+			}
+		}
+		return bestValue;
 	}
 
-	List<Integer> winningSpaces(BoardModel state) {
-		int p1Count = 0;
-		int p2Count = 0;
+	// Returns the number of turns needed to win by each player for this board
+	// (player1, player2)
+	public List<Integer> turnsToWin(BoardModel state) {
 		List<Integer> result = new ArrayList<Integer>();
-		List<Point> ws = new ArrayList<Point>(state.kLength);
-		for (int i = 0; i < state.width; ++i) {
-			for (int j = 0; j < state.height; ++j) {
-				// if the space previous is either not the same as current,
-				// empty, or OOB
-				// while the next thing is the same AND not OOB
-				// increment contiguous count
-				// if count greater than k, return the winner
-				// returns on first winning sequence found
-				// searches to the right and up
+		int p1best = 0;
+		int p2best = 0;
 
-				if (state.pieces[i][j] == 0) {
-					if (state.gravity)
-						break;// go to next column
+		// Horizontal Window method, size size.kLength
+		for (int j = 0 ; j < state.height ; j++) {
+			byte color = 0;
+			int spaces = 0;
+			int colorCount = 0;
+			int windowStart = 0;
+			for (int i = 0 ; i < state.width ; i++) {
+				if ( state.getSpace(i, j) == 0 ) {
+					spaces++;
+				}
+				// No color present or same color.
+				else if ( color == 0 || state.getSpace(i, j) == color) {
+					color = state.getSpace(i, j);
+					colorCount++;
+				}
+				// different color
+				else {
+					// Reset window AND count the new color
+					windowStart = i;
+					while (spaces-- > 0)
+						windowStart--;
+					color = state.getSpace(i, j); 
+					colorCount = 1;
+					spaces = 0;
+				}
+				// Scoring
+				if ( windowStart + state.kLength - 1 == i) {
+					if ( color == 1 )
+						p1best = Math.max(p1best, colorCount);
 					else
-						continue;// move up
-				}
+						p2best = Math.max(p2best, colorCount);
 
-				if (i - 1 < 0 || state.pieces[i - 1][j] != state.pieces[i][j]) { // horizontal
-					int count = 1;
-					while (i + count < state.width && (state.pieces[i][j] == state.pieces[i + count][j] || state.pieces[i + count][j] == 0) ) {
-						++count;
-						if (count == state.kLength) {
-							for (int k = 0; k < state.kLength; ++k)
-								ws.add(new Point(i + k, j));
-							if (state.pieces[i][j] == 1)
-								++p1Count;
-							else if(state.pieces[i][j] == 2)
-								++p2Count;
-							ws.clear();
-						}
-					}
-				}
-
-				if (i - 1 < 0 || j - 1 < 0 || state.pieces[i - 1][j - 1] != state.pieces[i][j]) { // diagonal
-																						// up,
-																						// (j-1<0)
-																						// needed
-																						// to
-																						// avoid
-																						// OOB
-					int count = 1;
-					while (i + count < state.width && j + count < state.height && (state.pieces[i][j] == state.pieces[i + count][j + count] || state.pieces[i + count][j + count] == 0) ) {
-						++count;
-						if (count == state.kLength) {
-							for (int k = 0; k < state.kLength; ++k)
-								ws.add(new Point(i + k, j + k));
-							if (state.pieces[i][j] == 1)
-								++p1Count;
-							else if(state.pieces[i][j] == 2)
-								++p2Count;
-							ws.clear();
-						}
-					}
-				}
-
-				if (i - 1 < 0 || j + 1 >= state.height || state.pieces[i - 1][j + 1] != state.pieces[i][j]) { // diagonal
-																							// down,
-																							// (j+1>=height)
-																							// needed
-																							// to
-																							// avoid
-																							// OOB
-					int count = 1;
-					while (i + count < state.width && j - count >= 0 && (state.pieces[i][j] == state.pieces[i + count][j - count] || state.pieces[i + count][j - count] == 0) ) {
-						++count;
-						if (count == state.kLength) {
-							for (int k = 0; k < state.kLength; ++k)
-								ws.add(new Point(i + k, j - k));
-							if (state.pieces[i][j] == 1)
-								++p1Count;
-							else if(state.pieces[i][j] == 2)
-								++p2Count;
-							ws.clear();
-						}
-					}
-				}
-
-				if (j - 1 < 0 || state.pieces[i][j - 1] != state.pieces[i][j]) { // vertical
-					int count = 1;
-					while (j + count < state.height && (state.pieces[i][j] == state.pieces[i][j + count] || state.pieces[i][j + count] == 0) ) {
-						++count;
-						if (count == state.kLength) {
-							for (int k = 0; k < state.kLength; ++k)
-								ws.add(new Point(i, j + k));
-							if (state.pieces[i][j] == 1)
-								++p1Count;
-							else if(state.pieces[i][j] == 2)
-								++p2Count;
-							ws.clear();
-						}
-					}
+					// Adjust the window on a score
+					if ( state.getSpace(windowStart, j) == 0 )
+						spaces--;
+					else if ( state.getSpace(windowStart, j) == color )
+						colorCount--;
+					windowStart++;
 				}
 			}
 		}
-		result.add(p1Count);
-		result.add(p2Count);
+
+
+		// Vertical Window method, size size.kLength
+		for (int i = 0 ; i < state.width ; i++) {
+			byte color = 0;
+			int spaces = 0;
+			int colorCount = 0;
+			int windowStart = 0;
+			for (int j = 0 ; j < state.height ; j++) {
+				if ( state.getSpace(i, j) == 0 ) {
+					spaces++;
+				}
+				// No color present or same color.
+				else if ( color == 0 || state.getSpace(i, j) == color) {
+					color = state.getSpace(i, j);
+					colorCount++;
+				}
+				// different color
+				else {
+					// Reset window AND count the new color
+					windowStart = j;
+					while (spaces-- > 0)
+						windowStart--;
+					color = state.getSpace(i, j); 
+					colorCount = 1;
+					spaces = 0;
+				}
+				// Scoring
+				if (windowStart + state.kLength - 1 == j) {
+					if ( color == 1 )
+						p1best = Math.max(p1best, colorCount);
+					else if ( color == 2 )
+						p2best = Math.max(p2best, colorCount);
+
+					// Adjust the window on a score
+					if ( state.getSpace(i, windowStart) == 0 )
+						spaces--;
+					else if ( state.getSpace(i, windowStart) == color )
+						colorCount--;
+					windowStart++;
+				}
+			}
+		}
+		result.add(p1best);
+		result.add(p2best);
 		return result;
 	}
+
+	//	List<Integer> winningSpaces(BoardModel state) {
+	//		int p1Count = 0;
+	//		int p2Count = 0;
+	//		List<Integer> result = new ArrayList<Integer>();
+	//		List<Point> ws = new ArrayList<Point>(state.kLength);
+	//		for (int i = 0; i < state.width; ++i) {
+	//			for (int j = 0; j < state.height; ++j) {
+	//				// if the space previous is either not the same as current,
+	//				// empty, or OOB
+	//				// while the next thing is the same AND not OOB
+	//				// increment contiguous count
+	//				// if count greater than k, return the winner
+	//				// returns on first winning sequence found
+	//				// searches to the right and up
+	//
+	//				if (state.pieces[i][j] == 0) {
+	//					if (state.gravity)
+	//						break;// go to next column
+	//					else
+	//						continue;// move up
+	//				}
+	//
+	//				if (i - 1 < 0 || state.pieces[i - 1][j] != state.pieces[i][j]) { // horizontal
+	//					int count = 1;
+	//					while (i + count < state.width && (state.pieces[i][j] == state.pieces[i + count][j] || state.pieces[i + count][j] == 0) ) {
+	//						++count;
+	//						if (count == state.kLength) {
+	//							for (int k = 0; k < state.kLength; ++k)
+	//								ws.add(new Point(i + k, j));
+	//							if (state.pieces[i][j] == 1)
+	//								++p1Count;
+	//							else if(state.pieces[i][j] == 2)
+	//								++p2Count;
+	//							ws.clear();
+	//						}
+	//					}
+	//				}
+	//
+	//				if (i - 1 < 0 || j - 1 < 0 || state.pieces[i - 1][j - 1] != state.pieces[i][j]) { // diagonal
+	//																						// up,
+	//																						// (j-1<0)
+	//																						// needed
+	//																						// to
+	//																						// avoid
+	//																						// OOB
+	//					int count = 1;
+	//					while (i + count < state.width && j + count < state.height && (state.pieces[i][j] == state.pieces[i + count][j + count] || state.pieces[i + count][j + count] == 0) ) {
+	//						++count;
+	//						if (count == state.kLength) {
+	//							for (int k = 0; k < state.kLength; ++k)
+	//								ws.add(new Point(i + k, j + k));
+	//							if (state.pieces[i][j] == 1)
+	//								++p1Count;
+	//							else if(state.pieces[i][j] == 2)
+	//								++p2Count;
+	//							ws.clear();
+	//						}
+	//					}
+	//				}
+	//
+	//				if (i - 1 < 0 || j + 1 >= state.height || state.pieces[i - 1][j + 1] != state.pieces[i][j]) { // diagonal
+	//																							// down,
+	//																							// (j+1>=height)
+	//																							// needed
+	//																							// to
+	//																							// avoid
+	//																							// OOB
+	//					int count = 1;
+	//					while (i + count < state.width && j - count >= 0 && (state.pieces[i][j] == state.pieces[i + count][j - count] || state.pieces[i + count][j - count] == 0) ) {
+	//						++count;
+	//						if (count == state.kLength) {
+	//							for (int k = 0; k < state.kLength; ++k)
+	//								ws.add(new Point(i + k, j - k));
+	//							if (state.pieces[i][j] == 1)
+	//								++p1Count;
+	//							else if(state.pieces[i][j] == 2)
+	//								++p2Count;
+	//							ws.clear();
+	//						}
+	//					}
+	//				}
+	//
+	//				if (j - 1 < 0 || state.pieces[i][j - 1] != state.pieces[i][j]) { // vertical
+	//					int count = 1;
+	//					while (j + count < state.height && (state.pieces[i][j] == state.pieces[i][j + count] || state.pieces[i][j + count] == 0) ) {
+	//						++count;
+	//						if (count == state.kLength) {
+	//							for (int k = 0; k < state.kLength; ++k)
+	//								ws.add(new Point(i, j + k));
+	//							if (state.pieces[i][j] == 1)
+	//								++p1Count;
+	//							else if(state.pieces[i][j] == 2)
+	//								++p2Count;
+	//							ws.clear();
+	//						}
+	//					}
+	//				}
+	//			}
+	//		}
+	//		result.add(p1Count);
+	//		result.add(p2Count);
+	//		return result;
+	//	}
 
 	@Override
 	public Point getMove(BoardModel state, int deadline) {
--- src/GroupAITest.java
+++ src/GroupAITest.java
@@ -1,4 +1,8 @@
+import static org.junit.Assert.assertEquals;
+
 import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,15 +17,19 @@ public class GroupAITest {
 	@Before
 	public void setUp() {
 		board = new BoardModel(10, 10, 4, false);
-		board = board.placePiece(new Point(0,0), (byte) 1);
-		board = board.placePiece(new Point(1,0), (byte) 1);
-		board = board.placePiece(new Point(2,0), (byte) 1);
-		board = board.placePiece(new Point(3,0), (byte) 1);
-		board = board.placePiece(new Point(1,1), (byte) 1);
+//		board = board.placePiece(new Point(4,0), (byte) 1);
+//		board = board.placePiece(new Point(5,0), (byte) 1);
+//		board = board.placePiece(new Point(6,0), (byte) 1);
+		board = board.placePiece(new Point(7,0), (byte) 2);
+		board = board.placePiece(new Point(8,0), (byte) 1);
+		board = board.placePiece(new Point(9,0), (byte) 1);
 		
 		board = board.placePiece(new Point(9,9), (byte) 2);
 		board = board.placePiece(new Point(9,8), (byte) 2);
+		board = board.placePiece(new Point(9,7), (byte) 2);
 		board = board.placePiece(new Point(8,8), (byte) 2);
+		board = board.placePiece(new Point(7,7), (byte) 2);
+		board = board.placePiece(new Point(6,6), (byte) 2);
 		System.out.println(board.toString());
 		
 		ai = new GroupAI((byte) 1, board);
@@ -29,7 +37,11 @@ public class GroupAITest {
 
 	@Test
 	public void winningSpacesTest() {
-		System.out.println(ai.winningSpaces(board));
+		List<Integer> f = new ArrayList<Integer>();
+		f.add(4);
+		f.add(3);
+		System.out.println(ai.turnsToWin(board));
+		assertEquals(f, ai.turnsToWin(board));
 	}
 
 }
