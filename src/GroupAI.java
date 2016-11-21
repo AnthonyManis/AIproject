974de7196d14ba8ccbeb7d1a0c97ac4761c2da59
AnthonyManis <anthonygmanis@gmail.com>
2016-11-20 21:27:27 -0800

    introducing InterruptableSearch subclass

--- src/GroupAI.java
+++ src/GroupAI.java
@@ -16,8 +16,12 @@ public class GroupAI extends CKPlayer {
 	final byte PLAYER1 = 1;
 	final byte PLAYER2 = 2;
 
-	public Point bestPoint = new Point(); // (i, j)
+	public Point bestPoint;
 	public Map<BoardModel, Point> map;
+
+	public BoardModel runState;
+	public int runDepth;
+	public InterruptableSearch s;
 
 	public GroupAI(byte player, BoardModel state) {
 		super(player, state);
@@ -25,307 +29,251 @@ public class GroupAI extends CKPlayer {
 		bestPoint.x = 0;
 		bestPoint.y = 0;
 		map = new HashMap<BoardModel, Point>();
+		bestPoint = new Point(); // (i, j)
+		s = new InterruptableSearch();
 	}
 
 	@Override
 	public Point getMove(BoardModel state) {
-
+			
 		while (state.hasMovesLeft()){
-			System.out.println("best " + search(state, 4, player));
+			System.out.println("best " + s.search(state, 4, player));
 			return new Point(bestPoint.x, bestPoint.y);
 		}
 		return null;
 	}
-	
+
 	@Override
 	public Point getMove(BoardModel state, int deadline) {
-		
-		// While deadline is not reached, keep calling search for deeper level
-		
-		return getMove(state);
+
+		// Make a new thread from this object
+		Thread t = new Thread(s);
+
+		// Time in ms used to return value if search doesn't return
+		// by the deadline
+		long timeMargin = 100;
+
+
+		// Setup initial search parameters
+		runState = state;
+		runDepth = 1;
+
+		t.start();
+
+		// Run search until deadline is reached
+		try {
+			t.join(deadline-timeMargin);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		// the latest search to return bestPoint is used
+		return bestPoint;
 	}
 
-	public int heuristic(BoardModel state) {
-		int result = 0;
-		int winner = state.winner();
-		if ( winner != -1 ) {
-			if ( winner == 1 ) {
-				result =  WIN_VALUE;
+	public class InterruptableSearch implements Runnable {
+
+
+
+		public void run() {
+			while (true) {
+				search(runState, runDepth++, player);
 			}
-			if ( winner == 2) {
-				result = LOSE_VALUE;
+		}
+
+		public int heuristic(BoardModel state) {
+			int result = 0;
+			int winner = state.winner();
+			if ( winner != -1 ) {
+				if ( winner == 1 ) {
+					result =  WIN_VALUE;
+				}
+				if ( winner == 2) {
+					result = LOSE_VALUE;
+				}
+				if ( winner == 0) {
+					result = DRAW_VALUE;
+				}
+				if ( player == PLAYER2 ) {
+					result *= -1;
+				}
+				return result;
 			}
-			if ( winner == 0) {
-				result = DRAW_VALUE;
+
+
+
+
+			ArrayList<ArrayList<Integer>> cont = waysToWin(state);
+			int player1total = 0, player2total = 0;
+			for ( int i = 1 ; i < state.kLength ; i++ ) {
+				int weight = (int) Math.pow(10, i-1);
+				player1total += cont.get(1).get(i) * weight;
+				player2total += cont.get(2).get(i) * weight;
 			}
-			if ( player == PLAYER2 ) {
+
+			result = player1total - player2total;
+			if (player == PLAYER2) {
 				result *= -1;
 			}
 			return result;
 		}
 
 
-
-
-		ArrayList<ArrayList<Integer>> cont = waysToWin(state);
-		int player1total = 0, player2total = 0;
-		for ( int i = 1 ; i < state.kLength ; i++ ) {
-			int weight = (int) Math.pow(10, i-1);
-			player1total += cont.get(1).get(i) * weight;
-			player2total += cont.get(2).get(i) * weight;
+		public byte nextPlayer(byte p) {
+			return (byte) p == (byte) 1 ? (byte) 2 : (byte) 1;
 		}
 
-		result = player1total - player2total;
-		if (player == PLAYER2) {
-			result *= -1;
-		}
-		return result;
-	}
-
-
-	public byte nextPlayer(byte p) {
-		return (byte) p == (byte) 1 ? (byte) 2 : (byte) 1;
-	}
-
-	public int search(BoardModel state, int depth, byte move) {
-		int k = state.kLength;
-		int v = 0;
-		int bestV = 0;
-		int alpha = -INF, beta = INF;
-		boolean validMoveFound = false;
-		for (int i = 0 ; i < state.width ; i++) {
-			for (int j = 0 ; j < state.height ; j++) {
-				Point p = new Point(i,j);
-				// If map.get(state) exists, search it first
-				// Explore map.get(state); (best point first)
-				if ( i == 0 && j == 0 && map.get(state) != null ) {
-					j--; // cover 0,0 with next iteration of for loop
-					p = map.get(state);
-				}
-				if (state.getSpace(p) == 0) {
-					v = minSearch(state.placePiece(p, move), depth-1, nextPlayer(move), alpha, beta);
-					alpha = Math.max(alpha, v);
-					// fallback in case we don't find anything we like
-					if ( !validMoveFound ) {
-						validMoveFound = true;
-						bestV = v;
-						bestPoint.x = p.x;
-						bestPoint.y = p.y;
-						map.put(state, bestPoint);
-					}
-					
-					if ( v > bestV ) {
-						bestV = v;
-						bestPoint.x = p.x;
-						bestPoint.y = p.y;
-						map.put(state, bestPoint);
-					}
-				}
-			}
-		}
-		return v;
-	}
-
-	public int maxSearch(BoardModel state, int depth, byte move, int alpha, int beta) {
-		if ( depth == 0 ) {
-			return heuristic(state);
-		}
-
-		int h = heuristic(state);
-		if ( h == DRAW_VALUE || h == WIN_VALUE || h == LOSE_VALUE ) {
-			return h;
-		}
-
-		Point bestMove = new Point();
-		boolean bestMoveFound = false;
-		for (int i = 0 ; i < state.width ; i++) {
-			for (int j = 0 ; j < state.height ; j++) {
-				Point p = new Point(i,j);
-				// If map.get(state) exists, search it first
-				// Explore map.get(state); (best point first)
-				if ( i == 0 && j == 0 && map.get(state) != null ) {
-					j--; // cover 0,0 with next iteration of for loop
-					p = map.get(state);
-				}
-				if (state.getSpace(p) == 0) {
-					int v = minSearch(state.placePiece(p, move), depth-1, nextPlayer(move), alpha, beta);
-					if ( !bestMoveFound ) {
-						bestMove = p;
-						bestMoveFound = true;
-					}
-					if ( v > alpha ) {
-						alpha = v;
-						bestMove = p;
-					}
-					if (alpha >= beta) {
-						// Put point into the map
-						map.put(state, bestMove);
-						return INF;
-					}
-				}
-			}
-		}
-		// Put point into the map
-		map.put(state, bestMove);
-		return alpha;
-	}
-
-	public int minSearch(BoardModel state, int depth, byte move, int alpha, int beta) {
-		if ( depth == 0 ) {
-			return heuristic(state);
-		}
-
-		int h = heuristic(state);
-		if ( h == DRAW_VALUE || h == WIN_VALUE || h == LOSE_VALUE ) {
-			return h;
-		}
-
-		Point bestMove = new Point();
-		boolean bestMoveFound = false;
-		for (int i = 0 ; i < state.width ; i++) {
-			for (int j = 0 ; j < state.height ; j++) {
-				Point p = new Point(i,j);
-				// If map.get(state) exists, search it first
-				// Explore map.get(state); (best point first)
-				if ( i == 0 && j == 0 && map.get(state) != null ) {
-					j--; // cover 0,0 with next iteration of for loop
-					p = map.get(state);
-				}
-				if (state.getSpace(p) == 0) {
-					int v = maxSearch(state.placePiece(p, move), depth-1, nextPlayer(move), alpha, beta);
-					if ( !bestMoveFound ) {
-						bestMove = p;
-						bestMoveFound = true;
-					}
-					if ( v < beta ) {
-						beta = v;
-						bestMove = p;
-					}
-					if (alpha >= beta) {
-						// Put point into the map
-						map.put(state, bestMove);
-						return -INF;
-					}
-				}
-			}
-		}
-		// Put point into the map
-		map.put(state, bestMove);
-		return beta;
-	}
-
-
-
-	// Returns the number of turns needed to win by each player for this board
-	// (player1, player2)
-	public ArrayList<ArrayList<Integer>> waysToWin(BoardModel state) {
-		ArrayList<Integer> p1totals = new ArrayList<Integer>(state.kLength+1);
-		ArrayList<Integer> p2totals = new ArrayList<Integer>(state.kLength+1);
-		for (int i = 0 ; i <= state.kLength ; i++) {
-			p1totals.add(0);
-			p2totals.add(0);
-		}
-		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-
-		// Horizontal windows
-		for (int j = 0 ; j < state.height ; j++) {
-			int player1 = 0, player2 = 0;
-			int windowStart= 0, windowSize = 0;
+		public int search(BoardModel state, int depth, byte move) {
+			int k = state.kLength;
+			int v = 0;
+			int bestV = 0;
+			int alpha = -INF, beta = INF;
+			boolean validMoveFound = false;
 			for (int i = 0 ; i < state.width ; i++) {
-				if ( windowSize < state.kLength )
-					windowSize++;
-
-				if ( state.getSpace(i, j) == 1 )
-					player1++;
-				else if ( state.getSpace(i, j) == 2)
-					player2++;
-
-				// Scoring
-				if ( windowSize == state.kLength ) {
-					if ( player1 == 0 ) {
-						int t = p2totals.get(player2) + 1;
-						p2totals.set(player2, t);
-					}						
-					if ( player2 == 0 ) {
-						int t = p1totals.get(player1) + 1;
-						p1totals.set(player1, t);
+				for (int j = 0 ; j < state.height ; j++) {
+					Point p = new Point(i,j);
+					// If map.get(state) exists, search it first
+					// Explore map.get(state); (best point first)
+					if ( i == 0 && j == 0 && map.get(state) != null ) {
+						j--; // cover 0,0 with next iteration of for loop
+						p = map.get(state);
 					}
+					if (state.getSpace(p) == 0) {
+						v = minSearch(state.placePiece(p, move), depth-1, nextPlayer(move), alpha, beta);
+						alpha = Math.max(alpha, v);
+						// fallback in case we don't find anything we like
+						if ( !validMoveFound ) {
+							validMoveFound = true;
+							bestV = v;
+							bestPoint.x = p.x;
+							bestPoint.y = p.y;
+						}
 
-					// Adjust window
-					if ( state.getSpace(windowStart, j) == 1 )
-						player1--;
-					else if ( state.getSpace(windowStart, j) == 2 )
-						player2--;
-					windowSize--;
-					windowStart++;
+						if ( v > bestV ) {
+							bestV = v;
+							bestPoint.x = p.x;
+							bestPoint.y = p.y;
+						}
+					}
 				}
 			}
+			map.put(state, bestPoint);
+			return v;
 		}
 
-		// Vertical windows
-		for (int i = 0 ; i < state.width ; i++) {
-			int player1 = 0, player2 = 0;
-			int windowStart = 0, windowSize = 0;
-			for (int j = 0 ; j < state.height ; j++) {
-				if ( windowSize < state.kLength ) {
-					windowSize++;
-				}
-
-				if ( state.getSpace(i, j) == 1 )
-					player1++;
-				else if ( state.getSpace(i, j) == 2 )
-					player2++;
-
-				// Scoring
-				if ( windowSize == state.kLength ) {
-					if ( player1 == 0 ) {
-						int t = p2totals.get(player2) + 1;
-						p2totals.set(player2, t);
-					}						
-					if ( player2 == 0 ) {
-						int t = p1totals.get(player1) + 1;
-						p1totals.set(player1, t);
-					}
-
-					// Adjust window
-					if ( state.getSpace(i, windowStart) == 1 )
-						player1--;
-					if ( state.getSpace(i, windowStart) == 2 )
-						player2--;
-					windowSize--;
-					windowStart++;
-				}
+		public int maxSearch(BoardModel state, int depth, byte move, int alpha, int beta) {
+			if ( depth == 0 ) {
+				return heuristic(state);
 			}
-		}
 
-		// Diagonal up windows
-		for (int j = 0 ; j < state.height ; j++) {
-			int player1 = 0, player2 = 0;
-			int windowSize = 0, windowStart = 0;
+			int h = heuristic(state);
+			if ( h == DRAW_VALUE || h == WIN_VALUE || h == LOSE_VALUE ) {
+				return h;
+			}
+
+			Point bestMove = new Point();
+			boolean bestMoveFound = false;
 			for (int i = 0 ; i < state.width ; i++) {
+				for (int j = 0 ; j < state.height ; j++) {
+					Point p = new Point(i,j);
+					// If map.get(state) exists, search it first
+					// Explore map.get(state); (best point first)
+					if ( i == 0 && j == 0 && map.get(state) != null ) {
+						j--; // cover 0,0 with next iteration of for loop
+						p = map.get(state);
+					}
+					if (state.getSpace(p) == 0) {
+						int v = minSearch(state.placePiece(p, move), depth-1, nextPlayer(move), alpha, beta);
+						if ( !bestMoveFound ) {
+							bestMove = p;
+							bestMoveFound = true;
+						}
+						if ( v > alpha ) {
+							alpha = v;
+							bestMove = p;
+						}
+						if (alpha >= beta) {
+							// Put point into the map
+							map.put(state, bestMove);
+							return INF;
+						}
+					}
+				}
+			}
+			// Put point into the map
+			map.put(state, bestMove);
+			return alpha;
+		}
 
-				// Skipping a lot of stuff that we don't need
-				if ( i != 0 && j != 0 )
-					break;
+		public int minSearch(BoardModel state, int depth, byte move, int alpha, int beta) {
+			if ( depth == 0 ) {
+				return heuristic(state);
+			}
 
-				int count = 0;
-				windowSize = 0;
-				windowStart = 0;
-				player1 = 0;
-				player2 = 0;
-				while (i + count < state.width && j + count < state.height ) {
-					if ( windowSize == state.kLength )
-						windowStart++;
+			int h = heuristic(state);
+			if ( h == DRAW_VALUE || h == WIN_VALUE || h == LOSE_VALUE ) {
+				return h;
+			}
 
-					if ( windowSize < state.kLength) {
+			Point bestMove = new Point();
+			boolean bestMoveFound = false;
+			for (int i = 0 ; i < state.width ; i++) {
+				for (int j = 0 ; j < state.height ; j++) {
+					Point p = new Point(i,j);
+					// If map.get(state) exists, search it first
+					// Explore map.get(state); (best point first)
+					if ( i == 0 && j == 0 && map.get(state) != null ) {
+						j--; // cover 0,0 with next iteration of for loop
+						p = map.get(state);
+					}
+					if (state.getSpace(p) == 0) {
+						int v = maxSearch(state.placePiece(p, move), depth-1, nextPlayer(move), alpha, beta);
+						if ( !bestMoveFound ) {
+							bestMove = p;
+							bestMoveFound = true;
+						}
+						if ( v < beta ) {
+							beta = v;
+							bestMove = p;
+						}
+						if (alpha >= beta) {
+							// Put point into the map
+							map.put(state, bestMove);
+							return -INF;
+						}
+					}
+				}
+			}
+			// Put point into the map
+			map.put(state, bestMove);
+			return beta;
+		}
+
+
+
+		// Returns the number of turns needed to win by each player for this board
+		// (player1, player2)
+		public ArrayList<ArrayList<Integer>> waysToWin(BoardModel state) {
+			ArrayList<Integer> p1totals = new ArrayList<Integer>(state.kLength+1);
+			ArrayList<Integer> p2totals = new ArrayList<Integer>(state.kLength+1);
+			for (int i = 0 ; i <= state.kLength ; i++) {
+				p1totals.add(0);
+				p2totals.add(0);
+			}
+			ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+
+			// Horizontal windows
+			for (int j = 0 ; j < state.height ; j++) {
+				int player1 = 0, player2 = 0;
+				int windowStart= 0, windowSize = 0;
+				for (int i = 0 ; i < state.width ; i++) {
+					if ( windowSize < state.kLength )
 						windowSize++;
-					}
 
-					if ( state.getSpace(i + count, j + count) == 1 )
+					if ( state.getSpace(i, j) == 1 )
 						player1++;
-					else if ( state.getSpace(i + count, j + count) == 2 )
+					else if ( state.getSpace(i, j) == 2)
 						player2++;
-
 
 					// Scoring
 					if ( windowSize == state.kLength ) {
@@ -339,44 +287,29 @@ public class GroupAI extends CKPlayer {
 						}
 
 						// Adjust window
-						if ( state.getSpace(i + windowStart, j + windowStart) == 1 )
+						if ( state.getSpace(windowStart, j) == 1 )
 							player1--;
-						if ( state.getSpace(i + windowStart, j + windowStart) == 2 )
+						else if ( state.getSpace(windowStart, j) == 2 )
 							player2--;
+						windowSize--;
+						windowStart++;
 					}
-					count++;
 				}
 			}
-		}
 
-		// Diagonal down windows
-		for (int j = 0 ; j < state.height ; j++) {
-			int player1 = 0, player2 = 0;
-			int windowSize = 0, windowStart = 0;
+			// Vertical windows
 			for (int i = 0 ; i < state.width ; i++) {
-
-				// Skipping a lot of stuff that we don't need
-				if ( i != 0 && j != state.height-1 )
-					break;
-
-				int count = 0;
-				windowSize = 0;
-				windowStart = 0;
-				player1 = 0;
-				player2 = 0;
-				while (i + count < state.width && j - count >=0) {
-					if ( windowSize == state.kLength )
-						windowStart++;
-
-					if ( windowSize < state.kLength) {
+				int player1 = 0, player2 = 0;
+				int windowStart = 0, windowSize = 0;
+				for (int j = 0 ; j < state.height ; j++) {
+					if ( windowSize < state.kLength ) {
 						windowSize++;
 					}
 
-					if ( state.getSpace(i + count, j - count) == 1 )
+					if ( state.getSpace(i, j) == 1 )
 						player1++;
-					else if ( state.getSpace(i + count, j - count) == 2 )
+					else if ( state.getSpace(i, j) == 2 )
 						player2++;
-
 
 					// Scoring
 					if ( windowSize == state.kLength ) {
@@ -390,20 +323,123 @@ public class GroupAI extends CKPlayer {
 						}
 
 						// Adjust window
-						if ( state.getSpace(i + windowStart, j - windowStart) == 1 )
+						if ( state.getSpace(i, windowStart) == 1 )
 							player1--;
-						if ( state.getSpace(i + windowStart, j - windowStart) == 2 )
+						if ( state.getSpace(i, windowStart) == 2 )
 							player2--;
+						windowSize--;
+						windowStart++;
 					}
-					count++;
 				}
 			}
+
+			// Diagonal up windows
+			for (int j = 0 ; j < state.height ; j++) {
+				int player1 = 0, player2 = 0;
+				int windowSize = 0, windowStart = 0;
+				for (int i = 0 ; i < state.width ; i++) {
+
+					// Skipping a lot of stuff that we don't need
+					if ( i != 0 && j != 0 )
+						break;
+
+					int count = 0;
+					windowSize = 0;
+					windowStart = 0;
+					player1 = 0;
+					player2 = 0;
+					while (i + count < state.width && j + count < state.height ) {
+						if ( windowSize == state.kLength )
+							windowStart++;
+
+						if ( windowSize < state.kLength) {
+							windowSize++;
+						}
+
+						if ( state.getSpace(i + count, j + count) == 1 )
+							player1++;
+						else if ( state.getSpace(i + count, j + count) == 2 )
+							player2++;
+
+
+						// Scoring
+						if ( windowSize == state.kLength ) {
+							if ( player1 == 0 ) {
+								int t = p2totals.get(player2) + 1;
+								p2totals.set(player2, t);
+							}						
+							if ( player2 == 0 ) {
+								int t = p1totals.get(player1) + 1;
+								p1totals.set(player1, t);
+							}
+
+							// Adjust window
+							if ( state.getSpace(i + windowStart, j + windowStart) == 1 )
+								player1--;
+							if ( state.getSpace(i + windowStart, j + windowStart) == 2 )
+								player2--;
+						}
+						count++;
+					}
+				}
+			}
+
+			// Diagonal down windows
+			for (int j = 0 ; j < state.height ; j++) {
+				int player1 = 0, player2 = 0;
+				int windowSize = 0, windowStart = 0;
+				for (int i = 0 ; i < state.width ; i++) {
+
+					// Skipping a lot of stuff that we don't need
+					if ( i != 0 && j != state.height-1 )
+						break;
+
+					int count = 0;
+					windowSize = 0;
+					windowStart = 0;
+					player1 = 0;
+					player2 = 0;
+					while (i + count < state.width && j - count >=0) {
+						if ( windowSize == state.kLength )
+							windowStart++;
+
+						if ( windowSize < state.kLength) {
+							windowSize++;
+						}
+
+						if ( state.getSpace(i + count, j - count) == 1 )
+							player1++;
+						else if ( state.getSpace(i + count, j - count) == 2 )
+							player2++;
+
+
+						// Scoring
+						if ( windowSize == state.kLength ) {
+							if ( player1 == 0 ) {
+								int t = p2totals.get(player2) + 1;
+								p2totals.set(player2, t);
+							}						
+							if ( player2 == 0 ) {
+								int t = p1totals.get(player1) + 1;
+								p1totals.set(player1, t);
+							}
+
+							// Adjust window
+							if ( state.getSpace(i + windowStart, j - windowStart) == 1 )
+								player1--;
+							if ( state.getSpace(i + windowStart, j - windowStart) == 2 )
+								player2--;
+						}
+						count++;
+					}
+				}
+			}
+			result.add(null);
+			result.add(p1totals);
+			result.add(p2totals);
+			return result;
 		}
-		result.add(null);
-		result.add(p1totals);
-		result.add(p2totals);
-		return result;
+
 	}
 
-	
 }
